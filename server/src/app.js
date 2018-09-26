254506f0c55a48783bf8ecf152e91525e6f72c2f
KassemKu <arabisc95@gmail.com>
2018-09-26 23:05:19 +0300

    set express cors morgan bodyparser

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,16 @@
-console.log('Done')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json)
+app.use(cors())
+const port = 3000
+
+app.get('/status', (req, res) => {
+  res.send({ message: 'Hello World' })
+})
+
+app.listen(port)
